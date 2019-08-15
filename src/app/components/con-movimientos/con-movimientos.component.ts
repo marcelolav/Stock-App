9b4ade50cd169c2afcd58f96cc152c4fc4a83a07
marcelolav <marcelo.lavandeira@gmail.com>
2019-08-15 11:24:11 -0300

    Consulta todos los movimientos.

--- src/app/components/con-movimientos/con-movimientos.component.ts
+++ src/app/components/con-movimientos/con-movimientos.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, TemplateRef, OnDestroy  } from '@angular/core';
+import { MovimientosService } from 'src/app/services/movimientos.service';
+// RXJS
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 @Component({
   selector: 'ang-con-movimientos',
   templateUrl: './con-movimientos.component.html',
   styleUrls: ['./con-movimientos.component.css']
 })
-export class ConMovimientosComponent implements OnInit {
-
-  constructor() { }
-
+export class ConMovimientosComponent implements OnInit, OnDestroy {
+  private unsubscribe = new Subject();
+  movimientos = [];
+  constructor( private movSer: MovimientosService) { 
+    this.movSer.getMovimientos()
+    .pipe(takeUntil(this.unsubscribe))
+    .subscribe(res => { this.movimientos = res; });
+  }
+  pruebaFuncion(dato) {
+    console.log(dato);
+  }
   ngOnInit() {
   }
-
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
 }
